b0bf947b0cdc0380ff943dee324d069895f51ae4
Phong Nguyen <nguyendoanphong.it@gmail.com>
2022-11-27 11:36:51 +0700

    Bulk Select

---
+++ src/EntityFrameworkCore.SqlServer.SimpleBulks/BulkSelect/BulkSelectBuilder.cs
@@ -0,0 +1,146 @@
+﻿using EntityFrameworkCore.SqlServer.SimpleBulks.Extensions;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace EntityFrameworkCore.SqlServer.SimpleBulks.BulkSelect
+{
+    public class BulkSelectBuilder<T>
+    {
+        private string _tableName;
+        private IEnumerable<string> _matchedColumns;
+        private IEnumerable<string> _columnNames;
+        private IDictionary<string, string> _dbColumnMappings;
+        private BulkOptions _options;
+        private readonly SqlConnection _connection;
+
+        public BulkSelectBuilder(SqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public BulkSelectBuilder<T> FromTable(string tableName)
+        {
+            _tableName = tableName;
+            return this;
+        }
+
+        public BulkSelectBuilder<T> WithMatchedColumns(string matchedColumn)
+        {
+            _matchedColumns = new List<string> { matchedColumn };
+            return this;
+        }
+
+        public BulkSelectBuilder<T> WithMatchedColumns(IEnumerable<string> matchedColumns)
+        {
+            _matchedColumns = matchedColumns;
+            return this;
+        }
+
+        public BulkSelectBuilder<T> WithMatchedColumns(Expression<Func<T, object>> matchedColumnsSelector)
+        {
+            var matchedColumn = matchedColumnsSelector.Body.GetMemberName();
+            _matchedColumns = string.IsNullOrEmpty(matchedColumn) ? matchedColumnsSelector.Body.GetMemberNames() : new List<string> { matchedColumn };
+            return this;
+        }
+
+        public BulkSelectBuilder<T> WithColumns(IEnumerable<string> columnNames)
+        {
+            _columnNames = columnNames;
+            return this;
+        }
+
+        public BulkSelectBuilder<T> WithColumns(Expression<Func<T, object>> columnNamesSelector)
+        {
+            _columnNames = columnNamesSelector.Body.GetMemberNames().ToArray();
+            return this;
+        }
+
+        public BulkSelectBuilder<T> WithDbColumnMappings(IDictionary<string, string> dbColumnMappings)
+        {
+            _dbColumnMappings = dbColumnMappings;
+            return this;
+        }
+
+        public BulkSelectBuilder<T> ConfigureBulkOptions(Action<BulkOptions> configureOptions)
+        {
+            _options = new BulkOptions();
+            if (configureOptions != null)
+            {
+                configureOptions(_options);
+            }
+            return this;
+        }
+
+        private string GetDbColumnName(string columnName)
+        {
+            if (_dbColumnMappings == null)
+            {
+                return columnName;
+            }
+
+            return _dbColumnMappings.ContainsKey(columnName) ? _dbColumnMappings[columnName] : columnName;
+        }
+
+        public IEnumerable<T> Execute(IEnumerable<T> machedValues)
+        {
+            var temptableName = "#" + Guid.NewGuid();
+
+            var dataTable = machedValues.ToDataTable(_matchedColumns);
+            var sqlCreateTemptable = dataTable.GenerateTableDefinition(temptableName);
+
+            var joinCondition = string.Join(" and ", _matchedColumns.Select(x =>
+            {
+                string collation = dataTable.Columns[x].DataType == typeof(string) ?
+                $" collate {Constants.Collation}" : string.Empty;
+                return $"a.[{GetDbColumnName(x)}]{collation} = b.[{x}]{collation}";
+            }));
+
+            var updateStatementBuilder = new StringBuilder();
+            updateStatementBuilder.AppendLine($"select {string.Join(", ", _columnNames.Select(x => CreateSelectStatement(x)))} ");
+            updateStatementBuilder.AppendLine($"from {_tableName} a join [{temptableName}] b on " + joinCondition);
+
+            _connection.EnsureOpen();
+
+            using (var createTemptableCommand = _connection.CreateTextCommand(null, sqlCreateTemptable))
+            {
+                createTemptableCommand.ExecuteNonQuery();
+            }
+
+            dataTable.SqlBulkCopy(temptableName, null, _connection, null, _options);
+
+            var results = new List<T>();
+
+            var properties = typeof(T).GetProperties().Where(prop => _columnNames.Contains(prop.Name)).ToList();
+
+            using var updateCommand = _connection.CreateTextCommand(null, updateStatementBuilder.ToString());
+            using var reader = updateCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                T obj = (T)Activator.CreateInstance(typeof(T));
+
+                foreach (var prop in properties)
+                {
+                    if (!Equals(reader[prop.Name], DBNull.Value))
+                    {
+                        prop.SetValue(obj, reader[prop.Name], null);
+                    }
+                }
+
+                results.Add(obj);
+
+            }
+
+            return results;
+        }
+
+        private string CreateSelectStatement(string colunmName)
+        {
+            return $"a.[{GetDbColumnName(colunmName)}] as [{colunmName}]";
+        }
+    }
+}
