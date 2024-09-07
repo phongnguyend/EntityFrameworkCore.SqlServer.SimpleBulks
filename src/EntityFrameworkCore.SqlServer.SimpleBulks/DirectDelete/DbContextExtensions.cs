54ef5a6e82ea622f43cb18041047fa6467258dbc
Phong Nguyen <nguyendoanphong.it@gmail.com>
2024-09-07 19:50:07 +0700

    (#21) Direct Delete

---
+++ src/EntityFrameworkCore.SqlServer.SimpleBulks/DirectDelete/DbContextExtensions.cs
@@ -0,0 +1,30 @@
+﻿using EntityFrameworkCore.SqlServer.SimpleBulks.BulkDelete;
+using EntityFrameworkCore.SqlServer.SimpleBulks.Extensions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace EntityFrameworkCore.SqlServer.SimpleBulks.DirectDelete
+{
+    public static class DbContextExtensions
+    {
+        public static BulkDeleteResult DirectDelete<T>(this DbContext dbContext, T data, Action<BulkDeleteOptions> configureOptions = null)
+        {
+            string tableName = dbContext.GetTableName(typeof(T));
+            var connection = dbContext.GetSqlConnection();
+            var transaction = dbContext.GetCurrentSqlTransaction();
+            var properties = dbContext.GetProperties(typeof(T));
+            var primaryKeys = properties
+                .Where(x => x.IsPrimaryKey)
+                .Select(x => x.PropertyName);
+            var dbColumnMappings = properties.ToDictionary(x => x.PropertyName, x => x.ColumnName);
+
+            return new BulkDeleteBuilder<T>(connection, transaction)
+                 .WithId(primaryKeys)
+                 .WithDbColumnMappings(dbColumnMappings)
+                 .ToTable(tableName)
+                 .ConfigureBulkOptions(configureOptions)
+                 .SingleDelete(data);
+        }
+    }
+}
