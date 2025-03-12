0fa36ef5d8e5f166d32bd5d249f2bb7e7a8c4ffa
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-03-12 18:15:55 +0700

    Support Client Generated Id

--- src/EntityFrameworkCore.SqlServer.SimpleBulks/BulkInsert/BulkInsertBuilder.cs
+++ src/EntityFrameworkCore.SqlServer.SimpleBulks/BulkInsert/BulkInsertBuilder.cs
@@ -105,6 +105,8 @@ public class BulkInsertBuilder<T>
         return _columnNameMappings.TryGetValue(columnName, out string value) ? value : columnName;
     }
 
+    private bool ReturnGeneratedId => !string.IsNullOrWhiteSpace(_outputIdColumn);
+
     public void Execute(IEnumerable<T> data)
     {
         if (data.Count() == 1)
@@ -114,7 +116,7 @@ public class BulkInsertBuilder<T>
         }
 
         DataTable dataTable;
-        if (string.IsNullOrWhiteSpace(_outputIdColumn))
+        if (!ReturnGeneratedId)
         {
             dataTable = data.ToDataTable(_columnNames);
 
@@ -132,6 +134,33 @@ public class BulkInsertBuilder<T>
             if (!columnsToInsert.Contains(_outputIdColumn))
             {
                 columnsToInsert.Add(_outputIdColumn);
+            }
+
+            dataTable = data.ToDataTable(columnsToInsert);
+
+            _connection.EnsureOpen();
+
+            Log($"Begin executing SqlBulkCopy. TableName: {_table.SchemaQualifiedTableName}");
+            dataTable.SqlBulkCopy(_table.SchemaQualifiedTableName, _columnNameMappings, _connection, _transaction, _options);
+            Log("End executing SqlBulkCopy.");
+            return;
+        }
+
+        var idProperty = typeof(T).GetProperty(_outputIdColumn);
+
+        if (_outputIdMode == OutputIdMode.ClientGenerated)
+        {
+            var columnsToInsert = _columnNames.Select(x => x).ToList();
+            if (!columnsToInsert.Contains(_outputIdColumn))
+            {
+                columnsToInsert.Add(_outputIdColumn);
+            }
+
+            var setIdDelegate = (Action<T, Guid>)Delegate.CreateDelegate(typeof(Action<T, Guid>), idProperty.GetSetMethod());
+
+            foreach (var row in data)
+            {
+                setIdDelegate(row, SequentialGuidGenerator.Next());
             }
 
             dataTable = data.ToDataTable(columnsToInsert);
@@ -190,8 +219,6 @@ public class BulkInsertBuilder<T>
         }
         Log("End merging temp table.");
 
-        var idProperty = typeof(T).GetProperty(_outputIdColumn);
-
         long idx = 0;
         foreach (var row in data)
         {
@@ -216,11 +243,25 @@ public class BulkInsertBuilder<T>
             insertStatementBuilder.AppendLine($"INSERT INTO {_table.SchemaQualifiedTableName} ({string.Join(", ", columnsToInsert.Select(x => $"[{GetDbColumnName(x)}]"))})");
             insertStatementBuilder.AppendLine($"VALUES ({string.Join(", ", columnsToInsert.Select(x => $"@{x}"))})");
         }
+        else if (ReturnGeneratedId && _outputIdMode == OutputIdMode.ClientGenerated)
+        {
+            if (!columnsToInsert.Contains(_outputIdColumn))
+            {
+                columnsToInsert.Add(_outputIdColumn);
+            }
+
+            var idProperty = typeof(T).GetProperty(_outputIdColumn);
+            var setIdDelegate = (Action<T, Guid>)Delegate.CreateDelegate(typeof(Action<T, Guid>), idProperty.GetSetMethod());
+            setIdDelegate(dataToInsert, SequentialGuidGenerator.Next());
+
+            insertStatementBuilder.AppendLine($"INSERT INTO {_table.SchemaQualifiedTableName} ({string.Join(", ", columnsToInsert.Select(x => $"[{GetDbColumnName(x)}]"))})");
+            insertStatementBuilder.AppendLine($"VALUES ({string.Join(", ", columnsToInsert.Select(x => $"@{x}"))})");
+        }
         else
         {
             insertStatementBuilder.AppendLine($"INSERT INTO {_table.SchemaQualifiedTableName} ({string.Join(", ", columnsToInsert.Select(x => $"[{GetDbColumnName(x)}]"))})");
 
-            if (!string.IsNullOrEmpty(_outputIdColumn))
+            if (ReturnGeneratedId)
             {
                 insertStatementBuilder.AppendLine($"OUTPUT inserted.[{GetDbColumnName(_outputIdColumn)}]");
             }
@@ -237,7 +278,7 @@ public class BulkInsertBuilder<T>
 
         _connection.EnsureOpen();
 
-        if (_options.KeepIdentity || string.IsNullOrEmpty(_outputIdColumn))
+        if (_options.KeepIdentity || !ReturnGeneratedId)
         {
             var affectedRow = insertCommand.ExecuteNonQuery();
         }
